f08cb22955c02a730d669eec971111fa9c80e598
mauricioecastillo27-blip <mauricioecastillo27@gmail.com>
2026-05-15 16:08:13 -0300

    Update calculadora.py
    le agregue un "WHILE" para retroalimentar la calculadora una vez termine una función, solo finaliza si y solo si el usuario lo decide

--- calculadora.py
+++ calculadora.py
@@ -5,13 +5,25 @@ def resta (n1,n2):
 def multiplicacion (n1,n2):
     return n1*n2
 #comienzo del programa
-n1=float(input("Ingrese el primer numero "))
-n2=float(input("Ingrese el segundo numero "))
-decision=int(input("Ingrese 1 si quiere suma, 2 para resta, 3 para multiplicación y 4 para división "))
-if decision==1:
-    print suma(n1,n2)
-elif decision==2:
-    print resta(n1,n2)
-elif decision==3:
-    print multiplicacion(n1,n2)
+while True: #para retroalimentacion del codigo
+    print("---seleccione una opcion---")
+    opcion=int(input(" sumar [ 1] restar [ 2 ] multiplicar [ 3 ] dividir [ 4 ] o salir [ 5 ] : "))
+    if opcion == 5:#termina solo si el usuario decide terminar, sino NO
+        print("---saliendo de la  calculadora...---")
+        break
+    if opcion==1 or opcion==2 or opcion==3 or opcion==4:
+        n1 = float(input("ingrese el primer numero: "))
+        n2 = float(input("ingrese el segundo numero: "))
+
+        #este bloque se conecta con las respectivas funciones
+        if opcion == 1:
+            print("resultado:  ", suma(n1, n2))
+        elif opcion == 2:
+            print("resultado:  ", resta(n1, n2))
+        elif opcion == 3:
+            print("resultado:  ", multiplicacion(n1, n2))
+        elif opcion == 4:
+            print("resultado:  ", division(n1, n2))
+        else:
+            print ("---opcion NO valida---")
 
